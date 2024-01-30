1b90178c319922b8dc022983b0f6ea019f7f7e31
Renan Victor <renanv.rodrigues00@gmail.com>
2024-01-30 19:46:30 -0300

    refactor: change helper library for mongodb-memory-server

--- src/infra/db/mondb/helpers/mongo-helper.ts
+++ src/infra/db/mondb/helpers/mongo-helper.ts
@@ -1,13 +1,21 @@
 import { MongoClient } from 'mongodb'
+import { MongoMemoryServer } from 'mongodb-memory-server'
 
 export const MongoHelper = {
-  client: null as unknown as MongoClient,
+  mongoServer: null as MongoMemoryServer | null,
+  client: null as MongoClient | null,
 
-  async connect (uri: string): Promise<void> {
-    this.client = await MongoClient.connect(global.__MONGO_URI__)
+  async connect (): Promise<void> {
+    this.mongoServer = await MongoMemoryServer.create()
+    this.client = await MongoClient.connect(this.mongoServer.getUri(), {})
   },
 
   async disconnect (): Promise<void> {
-    await this.client.close()
+    if (this.mongoServer) {
+      await this.mongoServer.stop()
+    }
+    if (this.client) {
+      await this.client.close()
+    }
   }
 }
