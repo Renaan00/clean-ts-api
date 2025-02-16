950ea68a9b9f62b70291b0fb00ebab32a284da91
Renan Victor <renanv.rodrigues00@gmail.com>
2025-02-15 23:15:37 -0300

    feat: ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controlles/login/login.ts
+++ src/presentation/controlles/login/login.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, serverError } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { EmailValidator } from '../signup/signup-protocols'
 
@@ -11,24 +11,27 @@ export class LoginController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body
-    if (!email) {
-      return await new Promise(resolve => { resolve(badRequest(new MissingParamError('email'))) })
-    }
-    if (!password) {
-      return await new Promise(resolve => { resolve(badRequest(new MissingParamError('password'))) })
-    }
-    const isValid = this.emailValidator.isValid(email)
-    if (!isValid) {
-      return await new Promise(resolve => { resolve(badRequest(new InvalidParamError('email'))) })
-    }
-
-    return {
-      statusCode: 200,
-      body: {
-        email: 'any_email@mail.com',
-        password: 'any_password'
+    try {
+      const { email, password } = httpRequest.body
+      if (!email) {
+        return await new Promise(resolve => { resolve(badRequest(new MissingParamError('email'))) })
       }
+      if (!password) {
+        return await new Promise(resolve => { resolve(badRequest(new MissingParamError('password'))) })
+      }
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return await new Promise(resolve => { resolve(badRequest(new InvalidParamError('email'))) })
+      }
+      return {
+        statusCode: 200,
+        body: {
+          email: 'any_email@mail.com',
+          password: 'any_password'
+        }
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
