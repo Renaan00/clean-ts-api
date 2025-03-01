c0b6072f3d1a63d019e7d5fda082ab067bdedf62
Renan Victor <renanv.rodrigues00@gmail.com>
2025-02-28 21:28:15 -0300

    refactor: move bcrypt-adapter to a folder

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,6 +1,6 @@
 import { SignUpController } from '../../../presentation/controlles/signup/signup'
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
-import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { AccountMongoRepository } from '../../../infra/db/mondb/account-repository/account'
 import { LogMongoRepository } from '../../../infra/db/mondb/log-repository/log'
 import { Controller } from '../../../presentation/protocols'
